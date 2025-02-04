11ca4b607060d0ce9e4ac1d6977ca8ac6c4ae67c
Andrew Peirs <peirs.andrew@gmail.com>
2025-02-04 19:18:24 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1
+        css={{
+          backgroundColor: "pink",
+          fontSize: "24px",
+        }}
+      >
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
